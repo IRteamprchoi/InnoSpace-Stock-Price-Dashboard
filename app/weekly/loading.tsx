8a2306e918fdda0bafa4dfde27f078ee12b8cb83
IRteamprchoi <prchoi.innospc@gmail.com>
2026-09-03 09:31:34 +0900

    Update loading.tsx

--- app/weekly/loading.tsx
+++ app/weekly/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div style={{ padding: "80px 20px", textAlign: "center", color: "#666" }}>
-      주간 데이터를 불러오는 중...
+      주간 데이터를 불러오는 중...(최대 24초 소요)
     </div>
   );
 }
